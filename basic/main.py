c82a08de3d133acf5fef974c646f2a9a864c5039
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2020-02-15 12:00:39 +0000

    added shebang in main.py

--- basic/main.py
+++ basic/main.py
@@ -3,7 +3,7 @@ from gpiozero import LED
 red = LED(22)
 amber = LED(27)
 green = LED(17)
-while True
-red.blink(1,1)
-amber.blink(2, 2)
-green.blink(3, 3)
+while True:
+    red.blink(1,1)
+    amber.blink(2, 2)
+    green.blink(3, 3)
