922fe9cec6670c41014f9c8ab0bfd258a943aeab
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2020-02-19 15:23:44 +0000

    added more code to main.py

--- python/gui/1.py
+++ python/gui/1.py
@@ -6,9 +6,13 @@ lights = TrafficLights(22, 27, 17)
 
 app = App("Traffic Lights controller", layout="grid")
 
-Text(app, "Red" , grid=[0, 0])
+Text(app, "Red", grid=[0, 0])
 PushButton(app, command=lights.red.on, text="on", grid=[1, 0])
 PushButton(app, command=lights.red.off, text="off", grid=[2, 0])
 PushButton(app, command=lights.red.blink, text="blink", grid=[3, 0])
+
+
+Text(app, "Amber", grid=[1, 0])
+
 
 app.display()
