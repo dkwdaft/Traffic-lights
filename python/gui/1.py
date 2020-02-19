4bbd8cba817dcdbf587047b5ed4aa449929fa58d
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2020-02-19 14:37:09 +0000

    added more code to main.py

--- python/gui/1.py
+++ python/gui/1.py
@@ -4,9 +4,9 @@ from gpiozero import TrafficLights
 lights = TrafficLights(22, 27, 17)
 
 
-app = App()
+app = App("Traffic Lights controller", layout="grid")
 
-Text(app, "Red")
-PushButton(app, command=lights.red.on, text="on")
-PushButton(app, command=lights.red.off, text="off")
+Text(app, "Red" , grid=[0, 0])
+PushButton(app, command=lights.red.on, text="on", grid=[1, 0])
+PushButton(app, command=lights.red.off, text="off", grid=[2, 0])
 app.display()
