6ffcfc841e12712b52701dfc43e11d6530f70f72
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2020-02-19 12:17:01 +0000

    added more code to main.py

--- python/basic/main-traffic-light-loop.py
+++ python/basic/main-traffic-light-loop.py
@@ -5,6 +5,18 @@ def trafic_lights_loop():
     sleep(0.5)
     green.off()
     sleep(0.5)
+    amber.on()
+    sleep(0.5)
+    amber.off()
+    sleep(0.5)
+    red.on()
+    sleep(0.5)
+    amber.on()
+    sleep(0.5)
+    red.off()
+    amber.off()
+    sleep(0.5)
+    green.on()
 red = LED(22)
 amber = LED(27)
 green = LED(17)
@@ -14,6 +26,6 @@ loop = input("Press enter to start traffic lights loop")
 loop = True
 
 while loop == True:
-
+    trafic_lights_loop()
 
 
