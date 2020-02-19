6fa3dd04e0270351147fed1cbbeeb0f753e61418
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2020-02-19 12:20:26 +0000

    added more code to main.py

--- python/basic/main-traffic-light-loop.py
+++ python/basic/main-traffic-light-loop.py
@@ -1,5 +1,9 @@
 from gpiozero import *
 from time import sleep
+red = LED(22)
+amber = LED(27)
+green = LED(17)
+
 def trafic_lights_loop():
     green.on()
     sleep(0.5)
@@ -17,15 +21,13 @@ def trafic_lights_loop():
     amber.off()
     sleep(0.5)
     green.on()
-red = LED(22)
-amber = LED(27)
-green = LED(17)
 
-while True
-loop = input("Press enter to start traffic lights loop")
-loop = True
 
-while loop == True:
-    trafic_lights_loop()
+while True:
+    loop = input("Press enter to start traffic lights loop")
+    loop = True
+
+    while loop == True:
+        trafic_lights_loop()
 
 
