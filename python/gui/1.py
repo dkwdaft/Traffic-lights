337f1f69da054da89933d3dbaafa3b7ee0d808ce
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2020-02-19 16:26:48 +0000

    added more code to 1.py

--- python/gui/1.py
+++ python/gui/1.py
@@ -1,8 +1,23 @@
 from guizero import App, Text, PushButton
-from gpiozero import TrafficLights
+from gpiozero import TrafficLights, LED
+red = LED(22)
+amber = LED(27)
+green = LED(17)
 
 lights = TrafficLights(22, 27, 17)
-
+def trafic_lights_loop():
+    green.off()
+    sleep(0.5)
+    amber.on()
+    sleep(0.5)
+    amber.off()
+    red.on()
+    sleep(0.5)
+    amber.on()
+    sleep(2)
+    red.off()
+    amber.off()
+    green.on()
 
 app = App("Traffic Lights controller", layout="grid")
 
@@ -27,5 +42,7 @@ PushButton(app, command=lights.on, text="on", grid=[1,3])
 PushButton(app, command=lights.off, text="off", grid=[2,3])
 PushButton(app, command=lights.blink, text="blink", grid=[3,3])
 
+Text(app, "Traffic light sequence", grid=[2,4])
+PushButton(app, command=trafic_lights_loop,text="run", grid=[2,5])
 
 app.display()
