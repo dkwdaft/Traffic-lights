1d4c93fca0d3eae91110d2b17d5737571c958409
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2020-02-19 15:25:01 +0000

    added more code to main.py

--- python/gui/1.py
+++ python/gui/1.py
@@ -12,7 +12,7 @@ PushButton(app, command=lights.red.off, text="off", grid=[2, 0])
 PushButton(app, command=lights.red.blink, text="blink", grid=[3, 0])
 
 
-Text(app, "Amber", grid=[1, 0])
+Text(app, "Amber", grid=[0, 1])
 
 
 app.display()
