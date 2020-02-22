55ebb4fa9ad711d492cdde47ebdbc973f68cf990
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2020-02-22 11:10:31 +0000

    added more code to 1.py

--- python/gui/1.py
+++ python/gui/1.py
@@ -1,11 +1,9 @@
 from guizero import App, Text, PushButton
 from gpiozero import TrafficLights
 from time import sleep
-red = LED(22)
-amber = LED(27)
-green = LED(17)
-
+# Set the pins the traffic light uses on the raspberry pi
 lights = TrafficLights(22, 27, 17)
+#Defines The Function Which Declares What The Program Should do For the traffic light loop
 def trafic_lights_loop():
     lights.green.off()
     sleep(0.5)
@@ -19,32 +17,32 @@ def trafic_lights_loop():
     lights.red.off()
     lights.amber.off()
     lights.green.on()
-
+#Sets app Title bar label
 app = App("Traffic Lights controller", layout="grid")
-
+#Creates button grid for Individual red light controller
 Text(app, "Red", grid=[0, 0])
 PushButton(app, command=lights.red.on, text="on", grid=[1, 0])
 PushButton(app, command=lights.red.off, text="off", grid=[2, 0])
 PushButton(app, command=lights.red.blink, text="blink", grid=[3, 0])
 
-
+#Creates button grid for Individual Amber light controller
 Text(app, "Amber", grid=[0, 1])
 PushButton(app, command=lights.amber.on, text="on", grid=[1,1])
 PushButton(app, command=lights.amber.off,text="off", grid=[2, 1])
 PushButton(app, command=lights.amber.blink, text="blink", grid=[3, 1])
-
+#Creates button grid for Individual Green light controller
 Text(app, "Green", grid=[0, 2])
 PushButton(app, command=lights.green.on, text="on", grid=[1,2])
 PushButton(app, command=lights.green.off, text="off", grid=[2,2])
 PushButton(app, command=lights.green.blink, text="blink", grid=[3,2])
-
+#Create button grid for All light control
 Text(app, "All", grid=[0,3])
 PushButton(app, command=lights.on, text="on", grid=[1,3])
 PushButton(app, command=lights.off, text="off", grid=[2,3])
 PushButton(app, command=lights.blink, text="blink", grid=[3,3])
-
+#Creates button And label grid For the traffic light sequence functions
 Text(app, "Traffic light sequence", grid=[2,4])
 PushButton(app, command=trafic_lights_loop, text="run loop", grid=[2, 5])
 
-
+#And finally sets The app to display on the screen
 app.display()
