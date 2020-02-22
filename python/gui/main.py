be48cf7c31ceda7ab0d6e084ae4e52913c2d035c
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2020-02-22 15:00:45 +0000

    added more code to maim.py

--- python/gui/main.py
+++ python/gui/main.py
@@ -46,7 +46,7 @@ PushButton(app, command=lights.blink, text="blink", grid=[3,3])
 Text(app, "Traffic light sequence", grid=[2,4])
 PushButton(app, command=trafic_lights_loop, grid=[2,6]
 #Creates button And label grid For the clear traffic light sequence functions
-Text("reset lights", grid=[3,6])
+Text(app,"reset lights", grid=[3,6])
 PushButton(app, command=lights.off, text="clear lights", grid=[3,5])
 
 #And finally sets The app to display on the screen
